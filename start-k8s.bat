1316576b9ac70cc947f2ef6a9ecba3e514d4d64d
GouTouZhao <zhaoziteng0624@gmail.com>
2026-06-29 13:23:29 +0800

    fix: add configmap and secret creation to start-k8s script

--- start-k8s.bat
+++ start-k8s.bat
@@ -1,6 +1,12 @@
 @echo off
-REM Create namespace
+REM Create namespace (ignore error if exists)
 kubectl create namespace forum
+
+REM Create ConfigMap from local config files
+kubectl create configmap forum-config --from-file=.\ForumBackend\config\ -n forum
+
+REM Create Secret for OSS (ignore error if exists or update it if needed, simple create for now)
+kubectl create secret generic forum-secret --from-literal=OSS_ACCESSKEYID=replace_me --from-literal=OSS_ACCESSKEYSECRET=replace_me --from-literal=OSS_ROLEARN=replace_me -n forum
 
 REM Deploy backend services
 kubectl apply -f .\ForumBackend\deployments\deploy\ -n forum
